0a6c57b0384bad7dd06f3d5c9c16704deeb083b9
Jerome PERRIN <jerome@MacBook-Pro-de-Jerome.local>
2026-01-16 21:01:11 +0100

    20260115-9

--- api/http.php
+++ api/http.php
@@ -39,7 +39,7 @@ function apply_cors(): void
 
     header('Access-Control-Allow-Origin: ' . $origin);
     header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-    header('Access-Control-Allow-Headers: Content-Type, X-Admin-Key');
+    header('Access-Control-Allow-Headers: Content-Type');
 
     if (($_SERVER['REQUEST_METHOD'] ?? '') === 'OPTIONS') {
         http_response_code(204);
